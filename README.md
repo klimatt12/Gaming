235cda6b36cdb1a1eaf6df0a70fd2ac848f154b3
unknown <klima.matt3@gmail.com>
2024-04-26 21:58:38 -0400

    should only alert when gaming session ends now, and not when it starts

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Utility to display stats from a gaming session
 
 
 # Datadog
-In the inital state, contains agent configuration files with the assumption the agent is already installed and running on a windows machine  
+In the initial state, contains agent configuration files with the assumption the agent is already installed and running on a windows machine  
 A docker image with config already completed will be added at a later date
 
 # Terraform 
--- Terraform/monitor.tf
+++ Terraform/monitor.tf
@@ -1,7 +1,7 @@
 resource "datadog_monitor" "process_alert_example" {
   name    = "Steam Gaming Session Ended"
   type    = "process alert"
-  message = "Steam gaming session has ended. Exact details can be found [here](https://us5.datadoghq.com/dashboard/kz5-sr2-6hb/steam-game-summary?fromUser=false&refresh_mode=sliding&from_ts=1714177030188&to_ts=1714180630188&live=true) @myemail@gmail.com"
+  message = "{{#is_alert}}Steam gaming session has ended. Exact details can be found [here](https://us5.datadoghq.com/dashboard/kz5-sr2-6hb/steam-game-summary?fromUser=false&refresh_mode=sliding&from_ts=1714177030188&to_ts=1714180630188&live=true) @myemail@gmail.com {{/is_alert}}"
   query   = "processes('\"steamapps\\common\"').rollup('count').last('5m') < 1"
   monitor_thresholds {
     critical          = 1.0
